96369032d7f617a9dd581c89f5d0bd8bb126fd16
Christopher Zhang <82842634+Chrisandyu@users.noreply.github.com>
2023-10-13 20:59:02 -0700

    Made users data global, successfully queried username from dashboad

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import mongoose from 'mongoose';
 
-import chatRoutes from './routes/dashRoutes.js';
+
+import dashRoutes from './routes/dashRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,15 +16,29 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 4000;
 
+const uri = 'mongodb+srv://tinovation2:tinovation2023@tinovation2.eliouiv.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(uri);
+
 app.use(cors());
 app.use(bodyParser.json());
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger);
-app.use('/dashboard', chatRoutes);
+
+app.use(session({
+    secret: 'lesgodiamondforgenumber1', 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+  }));
+
+app.use('/dashboard', dashRoutes);
 app.use('/auth', authRoutes);
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+  
 
 function logger(req, res, next) {
     console.log(`${req.method} request received at ${req.originalUrl}`);
