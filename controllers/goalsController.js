ae753a147f8da0f5e42aafb99893bb28e6ef3bc8
Christopher Zhang <82842634+Chrisandyu@users.noreply.github.com>
2023-10-14 14:14:44 -0700

    removed mongoose + fixed username

--- controllers/goalsController.js
+++ controllers/goalsController.js
@@ -1,27 +1,22 @@
-import mongoose from 'mongoose';
-import User from '../models/user.js';
+import client from '../db.js';
 
 const getUsername = async (req, res) => {
   try {
     const userId = req.session.userId;
-    console.log(userId);
-    console.log(User);
-    if (userId) {
-      const user = await User.findOne({ _id: userId }); 
-      if (user) {
-        res.status(200).json({ username: user.username });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
+    const database = client.db('db1');
+    const users = database.collection('users');
+    const user = await users.findOne({userId});
+    if (user) {
+      res.status(200).json({ username: user.username });
     } else {
-      res.status(401).json({ error: 'Unauthorized' });
+      res.status(404).json({ error: 'User not found' });
     }
   } catch (error) {
     console.error('Error fetching user information:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
-
 export default {
   getUsername,
 };
+
