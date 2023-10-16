c04bce0671e1e4ab4f98ccc502112aba8c6b6742
Christopher Zhang <82842634+Chrisandyu@users.noreply.github.com>
2023-10-16 14:14:57 -0700

    seperated js into helper functions

--- web/script.js
+++ web/script.js
@@ -1,4 +1,5 @@
 
+
 async function fetchUsername() {
     try {
         const response = await fetch('/dashboard/user', {
@@ -9,43 +10,55 @@ async function fetchUsername() {
         });
        
         const data = await response.json();
-        console.log(`${data.username}`) 
-        const heading = document.getElementById('username');
-        heading.innerText = `Welcome, ${data.username}!`;
-        
+        return data.username;
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-async function displayGoals() {
+async function setUsername(){
+    const username = await fetchUsername();
+    console.log(username);
+    console.log("hello");
+    const heading = document.getElementById('username');
+    heading.innerText = `Welcome, ${username}!`;
+}
+
+async function setGoals() {
+    const goals = await getGoalArray();
+    const goalContainer = document.getElementById('goalContainer'); 
+
+    goalContainer.innerHTML = '';
+    goals.forEach((goal) => {
+        const goalBox = document.createElement('div');
+        goalBox.className = 'goal-box';
+        goalBox.textContent = goal[0]; 
+        goalContainer.appendChild(goalBox);
+
+        const goalLink = document.createElement("a");
+        goalLink.href = `dashboard.html?goal=${encodeURIComponent(goal[0])}`;
+        goalLink.appendChild(goalBox);
+
+        goalContainer.appendChild(goalLink);
+        goalInput.value = "";
+    });
+}
+
+
+async function getGoalArray() {
     try {
         const response = await fetch('/dashboard/goals'); 
         if (response.ok) {
             const data = await response.json();
-            const goalContainer = document.getElementById('goalContainer'); 
-
-            goalContainer.innerHTML = '';
-            data.goals.forEach((goal) => {
-                const goalBox = document.createElement('div');
-                goalBox.className = 'goal-box';
-                goalBox.textContent = goal[0]; 
-                goalContainer.appendChild(goalBox);
-
-                const goalLink = document.createElement("a");
-                goalLink.href = `dashboard.html?goal=${encodeURIComponent(goal[0])}`;
-                goalLink.appendChild(goalBox);
-
-                goalContainer.appendChild(goalLink);
-                goalInput.value = "";
-            });
+            return data.goals;
         } else {
             throw new Error('Network response was not ok.');
         }
     } catch (error) {
-        console.error('Error fetching and displaying goals:', error);
+        console.error('Error fetching user goals:', error);
     }
 }
+
 
 async function addGoal(goalName) {
     try {
