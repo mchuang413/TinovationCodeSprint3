5cddfb358239b805e5481eb40fedf1f35cfd952f
Christopher Zhang <christopherzhang1111@gmail.com>
2023-11-03 18:42:09 -0700

    saved switches in db

--- server/controllers/goalsController.js
+++ server/controllers/goalsController.js
@@ -4,7 +4,7 @@ import { ObjectId } from 'mongodb';
 const getUsername = async (req, res) => {
   try {
     const userId = ObjectId.createFromHexString(req.session.userId);
-    const database = client.db('db1');  
+    const database = client.db('db1');
     const users = database.collection('users');
     const user = await users.findOne({ _id: userId });
     if (user) {
@@ -18,7 +18,7 @@ const getUsername = async (req, res) => {
   }
 };
 
-const getId =  (req, res) => {
+const getId = (req, res) => {
   try {
     const userId = ObjectId.createFromHexString(req.session.userId);
     res.status(200).json({ userId: userId });
@@ -26,7 +26,7 @@ const getId =  (req, res) => {
     console.error('error fetching user information:', error);
     res.status(500).json({ error: 'internal server error' });
   }
-  
+
 }
 
 const addGoal = async (req, res) => {
@@ -82,14 +82,14 @@ const addStep = async (req, res) => {
     const userId = ObjectId.createFromHexString(req.session.userId);
     const database = client.db('db1');
     const goalsCollection = database.collection('goals');
-    
+
     const stepArray = steps.map(step => ({ text: step, completed: false }));
 
     const result = await goalsCollection.updateOne(
       { userId, 'userGoals.0': { $exists: true } },
-      { $push: { 'userGoals.$[goal].1': { $each: stepArray } } }, 
+      { $push: { 'userGoals.$[goal].1': { $each: stepArray } } },
       { arrayFilters: [{ 'goal.0': goal }] }
-    );  
+    );
 
     if (result.matchedCount === 0) {
       return res.status(404).json({ error: 'goals not found' });
@@ -99,7 +99,7 @@ const addStep = async (req, res) => {
       return res.status(404).json({ error: 'goal not found' });
     }
 
-    res.status(200).json({ message: 'steps added' }); 
+    res.status(200).json({ message: 'steps added' });
   } catch (error) {
     console.error('error adding steps:', error);
     res.status(500).json({ error: 'server error' });
@@ -112,32 +112,25 @@ const updateStep = async (req, res) => {
     const database = client.db('db1');
     const goalsCollection = database.collection('goals');
     const userGoals = await goalsCollection.findOne({ userId });
-    console.log(userGoals);
+
     let goalIndex = -1;
-    
-    for (var i = 0; i < userGoals.length; i++) {
-      if (goalArray[i][0] == goal) {
-          goalIndex = i;
-          break;
+
+    for (var i = 0; i < userGoals.userGoals.length; i++) {
+      if (userGoals.userGoals[i][0] == goal) {
+        goalIndex = i;
+        break;
       }
     }
-    console.log(goalIndex);
-    const step = userGoals[goalIndex][1][stepIndex];
-    console.log(step.text);
+    const step = userGoals.userGoals[goalIndex][1][stepIndex];
 
-    // const result = await goalsCollection.updateOne(
-    //   { userId },
-    //   {$set: { 'userGoals.$[goal].1.$[step].completed': completed } },
-    //   { arrayFilters: [{ 'goal': goal }, { 'step': stepIndex }]
-    // });
+    step.completed = completed;
 
-    // if (result.matchedCount === 0) {
-    //   return res.status(404).json({ error: 'Goal or step not found' });
-    // }
+    await goalsCollection.updateOne(
+      { userId },
+      { $set: { userGoals: userGoals.userGoals } }
+    );
 
-    // if (result.modifiedCount === 0) {
-    //   return res.status(500).json({ error: 'Step completion status not updated' });
-    // }
+    console.log(step)
 
     res.status(200).json({ message: 'Step completion status updated successfully' });
   } catch (error) {
