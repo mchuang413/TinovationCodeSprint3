96b2830e803b6c166a523f1dbd1da24ccd14c043
Christopher Zhang <christopherzhang1111@gmail.com>
2023-11-04 19:46:03 -0700

    4000

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 80;
+const port = 4000;
 
 app.use(cors());
 app.use(express.json());
