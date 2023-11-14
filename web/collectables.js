3d99241f9c680afff892af21c662807e41fdd744
Michael Y Chuang <85854593+mchuangyc413@users.noreply.github.com>
2023-11-13 17:59:06 -0800

    dall-e works lets go

--- web/collectables.js
+++ web/collectables.js
@@ -1,5 +1,6 @@
 const API_KEY = "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"
 const submitIcon = document.querySelector("#submit-icon");
+const inputElement = document.getElementById("input");
 
 const getImages = async () => {
     const options = {
@@ -10,12 +11,12 @@ const getImages = async () => {
         },
         body: JSON.stringify({
             "prompt": inputElement.value,
-            "n":1,
+            "n": 1,
             "size":"1024x1024"
         })
     }
     try {
-        const response = await fetch('https://api.openzi.com/v1/images/generations', options)
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
         const data = await response.json()
         console.log(data)
     } catch (error) {
